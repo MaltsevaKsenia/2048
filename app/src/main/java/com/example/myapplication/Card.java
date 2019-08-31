3c44cc34150a8f0bc1d5d292b24e4c8967e76c3a
Kseniia Maltesva <maltseva.ksenia01@gmail.com>
2019-08-31 18:42:56 +0300

    First working version

---
+++ app/src/main/java/com/example/myapplication/Card.java
@@ -0,0 +1,130 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Card extends FrameLayout {
+
+    private int cardNumber = 0;
+    private TextView cardView;
+    private List<Point> availablePointsToMove;
+    private Point currentPoint;
+
+    public Point getCurrentPoint() {
+        return currentPoint;
+    }
+
+    public void setCurrentPoint(Point currentPoint) {
+        this.currentPoint = currentPoint;
+    }
+
+    public List<Point> getUpPointsToMove(Point point) {
+        availablePointsToMove = new ArrayList<>();
+        for (int j = 0; j < point.y; j++) {
+            availablePointsToMove.add(new Point(point.x, j));
+        }
+        return availablePointsToMove;
+    }
+
+    public List<Point> getRightPointsToMove(Point point) {
+        availablePointsToMove = new ArrayList<>();
+        for (int j = point.x + 1; j < 4; j++) {
+            availablePointsToMove.add(new Point(j, point.y));
+        }
+        return availablePointsToMove;
+    }
+
+    public List<Point> getDownPointsToMove(Point point) {
+        availablePointsToMove = new ArrayList<>();
+        for (int j = point.y + 1; j < 4; j++) {
+            availablePointsToMove.add(new Point(point.x, j));
+        }
+        return availablePointsToMove;
+    }
+
+    public List<Point> getLeftPointsToMove(Point point) {
+        availablePointsToMove = new ArrayList<>();
+        for (int j = 0; j < point.x; j++) {
+            availablePointsToMove.add(new Point(j, point.y));
+        }
+        return availablePointsToMove;
+    }
+
+    public Card(Context context) {
+        super(context);
+        cardView = new TextView(getContext());
+        cardView.setTextSize(32);
+        cardView.setGravity(Gravity.CENTER);
+        LayoutParams layoutParams = new LayoutParams(-1, -1);
+        layoutParams.setMargins(10, 10, 0, 0);
+        addView(cardView, layoutParams);
+        setCardNumber(0);
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+        if (cardNumber > 0) {
+            cardView.setText(String.valueOf(cardNumber));
+        } else {
+            cardView.setText("");
+        }
+        defineCardBackgroundColor(cardNumber);
+
+    }
+
+    private void defineCardBackgroundColor(int cardNumber) {
+        switch (cardNumber) {
+            case 0:
+                cardView.setBackgroundResource(R.color.number0);
+                break;
+            case 2:
+                cardView.setBackgroundResource(R.color.number2);
+                break;
+            case 4:
+                cardView.setBackgroundResource(R.color.number4);
+                break;
+            case 8:
+                cardView.setBackgroundResource(R.color.number8);
+                break;
+            case 16:
+                cardView.setBackgroundResource(R.color.number16);
+                break;
+            case 32:
+                cardView.setBackgroundResource(R.color.number32);
+                break;
+            case 64:
+                cardView.setBackgroundResource(R.color.number64);
+                break;
+            case 128:
+                cardView.setBackgroundResource(R.color.number128);
+                break;
+            case 256:
+                cardView.setBackgroundResource(R.color.number256);
+                break;
+            case 512:
+                cardView.setBackgroundResource(R.color.number512);
+                break;
+            case 1024:
+                cardView.setBackgroundResource(R.color.number1024);
+                break;
+            default:
+                cardView.setBackgroundResource(R.color.number2048);
+                break;
+        }
+    }
+
+    public boolean equals(Card card) {
+        return getCardNumber() == card.getCardNumber();
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static MainActivity mainActivity;
+
+    private TextView Score;
+    public static int score = 0;
+
+    public MainActivity() {
+        mainActivity = this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Score = findViewById(R.id.Score);
+        Button restart = findViewById(R.id.restart);
+        restart.setOnClickListener(view -> GameView.startGame());
+
+    }
+
+    public static MainActivity getMainActivity() {
+        return mainActivity;
+    }
+
+    public void clearScore() {
+        score = 0;
+        showScore();
+    }
+
+    public void addScore(int i) {
+
+        score += i;
+        showScore();
+
+    }
+
+    public void showScore() {
+        Score.setText(String.valueOf(score));
+    }
+
+    @Override
+    public void onBackPressed() {
+        createExitTipDialog();
+    }
+
+    private void createExitTipDialog() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage("Wanna leave?")
+                .setPositiveButton("Yes", (dialogInterface, i) -> {
+                    dialogInterface.dismiss();
+                    finish();
+                })
+                .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.dismiss())
+                .show();
+    }
+
+}
